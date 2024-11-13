80a8e21134990675eb93e34059e73e4f2c218716
doyourbest <creative555.dev@gmail.com>
2024-11-13 02:17:52 -0600

    [update] update sidebar component
    Co-Authored-By: jamest33 <182724236+jamest33@users.noreply.github.com>

--- src/sections/Sidebar.tsx
+++ src/sections/Sidebar.tsx
@@ -1,6 +1,10 @@
 "use client";
+import Link from "next/link";
 import classNames from "classnames";
 import { BsLayoutTextSidebarReverse } from "react-icons/bs";
+import { MdOutlineSpaceDashboard } from "react-icons/md";
+import { GoOrganization } from "react-icons/go";
+import { IoSettingsOutline } from "react-icons/io5";
 
 import { useEnv } from "@/contexts/EvnContext";
 import Logo from "@/components/Logo";
@@ -24,16 +28,41 @@ const Sidebar = () => {
         <div className="p-4">
           <Logo />
         </div>
-        <div className="p-4 flex flex-col gap-6">
-          <p>Dashboard</p>
-          <div className="flex flex-col gap-4">
-            <p>Organizations</p>
-            <div className="indent-4 space-y-2 text-sm">
-              <p>Requesting Demo</p>
-              <p>Allowed Organizations</p>
+        <div className="p-4 flex flex-col gap-2 text-gray-900">
+          <Link
+            href={"/dashboard"}
+            className="p-2 flex flex-row items-center gap-2 cursor-pointer rounded-sm hover:bg-gray-200"
+          >
+            <MdOutlineSpaceDashboard className="w-5 h-5" />
+            <span>Dashboard</span>
+          </Link>
+          <div className="flex flex-col">
+            <Link
+              href={"/orgs"}
+              className="p-2 flex flex-row items-center gap-2 cursor-pointer rounded-sm hover:bg-gray-200"
+            >
+              <GoOrganization className="w-5 h-5" />
+              Organizations
+            </Link>
+            <div className="flex flex-col indent-6 text-sm">
+              <Link
+                href={"/orgs/request"}
+                className="p-2 cursor-pointer rounded-sm hover:bg-gray-200"
+              >
+                Requesting Demo
+              </Link>
+              <Link
+                href={"/orgs/allowed"}
+                className="p-2 cursor-pointer rounded-sm hover:bg-gray-200"
+              >
+                Allowed Organizations
+              </Link>
             </div>
           </div>
-          <p>Settings</p>
+          <p className="p-2 flex flex-row items-center gap-2 cursor-pointer rounded-sm hover:bg-gray-200">
+            <IoSettingsOutline className="w-5 h-5" />
+            Settings
+          </p>
         </div>
       </div>
     </>

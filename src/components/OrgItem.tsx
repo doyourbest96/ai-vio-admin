5fcd324c6e4eacea4e6210af40045f6143f31c13
doyourbest <creative555.dev@gmail.com>
2024-11-13 02:22:36 -0600

    [update] update org item using format function
    Co-Authored-By: jamest33 <182724236+jamest33@users.noreply.github.com>

--- src/components/OrgItem.tsx
+++ src/components/OrgItem.tsx
@@ -1,6 +1,7 @@
 import { TrashIcon } from "@heroicons/react/24/outline";
 import ToggleButton from "@/components/extends/ToggleButton";
 import { OrgModel } from "@/services/orgService";
+import { getRelativeTime } from "@/utils/format";
 
 const OrgItem = ({ org, handleUpdate, handleDelete }: { org: OrgModel, handleUpdate: (orgData: OrgModel) => void, handleDelete: (orgId: string) => void }) => {
   return (
@@ -13,7 +14,7 @@ const OrgItem = ({ org, handleUpdate, handleDelete }: { org: OrgModel, handleUpd
           {org.size}
         </td>
         <td className="whitespace-nowrap py-3 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-3">
-          {org.createdAt}
+          {getRelativeTime(org.createdAt ?? "")}
         </td>
         <td className="whitespace-nowrap py-3 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-3">
           <ToggleButton
@@ -44,8 +45,8 @@ const OrgItem = ({ org, handleUpdate, handleDelete }: { org: OrgModel, handleUpd
           >
             <TrashIcon className="w-5 h-5 stroke-red-500" />
           </div>
-          {org.dmlType === "update" && (
-            <span className="absolute right-1 top-1 z-10 bg-green-500 text-white rounded-full px-2 py-1 text-xs">
+          {org.dmlType === "insert" && (
+            <span className="absolute right-2 top-2 z-10 bg-green-500 text-white rounded-full px-2 py-1 text-xs">
               New
             </span>
           )}

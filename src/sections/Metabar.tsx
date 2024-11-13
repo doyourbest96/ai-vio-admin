07bbffe8530f3cb83d755ef7430c9ae8c7e8257c
doyourbest <creative555.dev@gmail.com>
2024-11-12 23:00:29 -0600

    [feat] add metabar component
    Co-Authored-By: jamest33 <182724236+jamest33@users.noreply.github.com>

---
+++ src/sections/Metabar.tsx
@@ -0,0 +1,11 @@
+const Metabar = () => {
+  return (
+    <>
+      <div className="w-1/4 min-w-52 h-full hidden justify-center items-center border-l">
+        Metabar
+      </div>
+    </>
+  );
+};
+
+export default Metabar;
